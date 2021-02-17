0e4947f1d0be0371299e05fba385108c25fb1b0e
Joey Whalen <jtktwhalen@gmail.com>
2021-02-17 17:20:41 -0500

    begin create PersonForm Object

---
+++ src/main/java/com/example/validatingforminput/PersonForm.java
@@ -0,0 +1,17 @@
+package com.example.validatingforminput;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class PersonForm {
+
+	@NotNull
+	@Size(min=2, max=30)
+	private String name;
+
+	@NotNull
+	@Min(18)
+	private Integer age;
+	
+}
