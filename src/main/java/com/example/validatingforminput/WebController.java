fe9755d008202c9351c0fc7ea8ad64b1c7d0ff1f
Joey Whalen <jtktwhalen@gmail.com>
2021-02-17 20:22:30 -0500

    updated webController

--- src/main/java/com/example/validatingforminput/WebController.java
+++ src/main/java/com/example/validatingforminput/WebController.java
@@ -1,5 +1,11 @@
 package com.example.validatingforminput;
 
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -7,5 +13,20 @@ public class WebController implements WebMvcConfigurer{
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/results").setViewName("results");
+	}
+	
+	@GetMapping("/")
+	public String showForm(PersonForm personForm) {
+		return "form";
+	}
+	
+	@PostMapping("/")
+	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			return "form";
+		}
+		
+		return "redirect:/results";
 	}
 }
